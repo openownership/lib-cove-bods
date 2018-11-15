d8d8735259ca79bcf7ed0b0a1d9d1f3cc3815b7c
James Baster <james.baster@opendataservices.coop>
2018-11-15 15:01:02 +0000

    Bundle schema into lib to get round a ref problem

--- libcovebods/config.py
+++ libcovebods/config.py
@@ -1,10 +1,15 @@
+import os
 from libcove.config import LIB_COVE_CONFIG_DEFAULT, LibCoveConfig
 
 LIB_COVE_BODS_CONFIG_DEFAULT = LIB_COVE_CONFIG_DEFAULT.copy()
 
+_schema_folder = os.path.join(os.path.dirname(
+    os.path.realpath(__file__)), '..', 'data'
+)
+
 LIB_COVE_BODS_CONFIG_DEFAULT.update({
-    'schema_url': 'https://raw.githubusercontent.com/openownership/data-standard/schema-beta-2/schema/bods-package.json', # noqa
-    'schema_url_host': 'https://raw.githubusercontent.com/openownership/data-standard/schema-beta-2/schema/',
+    'schema_url': os.path.join(_schema_folder, 'schema.json'),
+    'schema_url_host': _schema_folder,
 })
 
 
