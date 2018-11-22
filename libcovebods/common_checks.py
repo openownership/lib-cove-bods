dd1fea0af920347f9f47244c1731d17944d6b40e
James Baster <james.baster@opendataservices.coop>
2018-11-22 14:06:34 +0000

    Cove Statistic: Number of Entities, Number of Persons, Number of ownership-or-control statements

--- libcovebods/common_checks.py
+++ libcovebods/common_checks.py
@@ -1,4 +1,5 @@
 from libcove.lib.common import common_checks_context
+from libcovebods.lib.common_checks import get_statistics
 
 
 def common_checks_bods(context, upload_dir, json_data, schema_obj):
@@ -6,5 +7,9 @@ def common_checks_bods(context, upload_dir, json_data, schema_obj):
     common_checks = common_checks_context(upload_dir, json_data, schema_obj, 'bods-schema.json', context)
 
     context.update(common_checks['context'])
+
+    context.update({
+      'statistics': get_statistics(json_data)
+    })
 
     return context
