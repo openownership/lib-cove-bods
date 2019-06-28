a1e91617e00f8edb3fd04b1fbfdb005b93ff9585
James Baster <james.baster@opendataservices.coop>
2019-06-28 15:43:22 +0100

    Refactor code so some code is in a handly library function we'll use soon

--- libcovebods/schema.py
+++ libcovebods/schema.py
@@ -4,23 +4,14 @@ from libcove.lib.common import SchemaJsonMixin
 class SchemaBODS(SchemaJsonMixin):
 
     def __init__(self, json_data=None, lib_cove_bods_config=None):
-        # The default schema we use ...
-        self.release_pkg_schema_url = lib_cove_bods_config.config['schema_url']
-        self.schema_host = lib_cove_bods_config.config['schema_url_host']
-        self.schema_version = lib_cove_bods_config.config['schema_version']
-        # ... unless the data specifies a version.
+        self.lib_cove_bods_config = lib_cove_bods_config
         if isinstance(json_data, list) and len(json_data) > 0:
-            statement = json_data[0]
-            if isinstance(statement, dict) \
-                    and 'publicationDetails' in statement \
-                    and isinstance(statement['publicationDetails'], dict) \
-                    and 'bodsVersion' in statement['publicationDetails'] \
-                    and statement['publicationDetails']['bodsVersion']:
-                version = statement['publicationDetails']['bodsVersion']
-                if version in lib_cove_bods_config.config['schema_versions']:
-                    self.release_pkg_schema_url = lib_cove_bods_config.config['schema_versions'][version]['schema_url']
-                    self.schema_host = lib_cove_bods_config.config['schema_versions'][version]['schema_url_host']
-                    self.schema_version = version
+            self.schema_version, self.release_pkg_schema_url, self.schema_host = \
+                self.get_schema_version_of_statement(json_data[0])
+        else:
+            self.release_pkg_schema_url = lib_cove_bods_config.config['schema_url']
+            self.schema_host = lib_cove_bods_config.config['schema_url_host']
+            self.schema_version = lib_cove_bods_config.config['schema_version']
 
     def get_entity_statement_types_list(self):
         for statement_schema in self._release_pkg_schema_obj['items']['oneOf']:
@@ -36,3 +27,19 @@ class SchemaBODS(SchemaJsonMixin):
         for statement_schema in self._release_pkg_schema_obj['items']['oneOf']:
             if statement_schema['properties']['statementType']['enum'][0] == 'ownershipOrControlStatement':
                 return statement_schema['properties']['interests']['items']['properties']['type']['enum']
+
+    def get_schema_version_of_statement(self, statement):
+        #  Does the data specify a version?
+        if isinstance(statement, dict) \
+                and 'publicationDetails' in statement \
+                and isinstance(statement['publicationDetails'], dict) \
+                and 'bodsVersion' in statement['publicationDetails'] \
+                and statement['publicationDetails']['bodsVersion']:
+            version = statement['publicationDetails']['bodsVersion']
+            if version in self.lib_cove_bods_config.config['schema_versions']:
+                return version, self.lib_cove_bods_config.config['schema_versions'][version]['schema_url'], \
+                       self.lib_cove_bods_config.config['schema_versions'][version]['schema_url_host']
+
+        # In which case, default schema
+        return self.lib_cove_bods_config.config['schema_version'], self.lib_cove_bods_config.config['schema_url'], \
+            self.lib_cove_bods_config.config['schema_version']
