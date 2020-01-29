848ee6a0068ac621c96cd017a2f7d4194a86c467
Steve Day <steve@openownership.org>
2020-01-29 14:58:02 +0000

    Only declare direct dependencies in setup.py
    An update to lib-cove showed me that most of what we're requiring here
    is actually required by lib-cove and so doesn't need to be explicitly
    required again.
    
    Django is an exception because this uses format_html directly.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='libcovebods',
-    version='0.5.0',
+    version='0.6.0',
     author='Open Data Services',
     author_email='code@opendataservices.coop',
     url='https://github.com/open-contracting/lib-cove-ocds',
@@ -10,20 +10,11 @@ setup(
     packages=find_packages(),
     long_description='A data review library',
     install_requires=[
-        'jsonref',
-        'jsonschema<2.7',
-        # Required for jsonschema to validate URIs
         'rfc3987',
-        # Required for jsonschema to validate date-time
         'strict-rfc3339',
-        'CommonMark',
         'Django',
-        'bleach',
-        'requests',
-        'json-merge-patch',
-        'cached-property',
         'python-dateutil',
-        'libcove>=0.6.0'
+        'libcove>=0.13.0'
     ],
     classifiers=[
             'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
