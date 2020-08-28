3ec8a785703baaefc889d5bccef33d40fc46b14f
Ben Webb <ben.webb@opendataservices.coop>
2020-08-28 10:07:45 +0000

    release: Make 0.8.0 release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+## [0.8.0] - 2020-08-28
+
 ### Changed
 
 - Rename schema object attributes to remove word "release", as in latest lib-cove
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='libcovebods',
-    version='0.7.0',
+    version='0.8.0',
     author='Open Data Services',
     author_email='code@opendataservices.coop',
     url='https://github.com/openownership/lib-cove-bods',
