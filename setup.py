5b58ea35ee6a72eb4b9dcd2c5d08b2a080783a05
Ben Webb <ben.webb@opendataservices.coop>
2021-01-27 11:34:38 +0000

    Release 0.9.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='libcovebods',
-    version='0.8.1',
+    version='0.9.0',
     author='Open Data Services',
     author_email='code@opendataservices.coop',
     url='https://github.com/openownership/lib-cove-bods',
