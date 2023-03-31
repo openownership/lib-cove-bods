9ee1de5d25cccd8c2970be5ce642da857fb46b29
James B <james.baster@opendataservices.coop>
2023-03-31 15:23:04 +0100

    MAKE STATS WORK

--- libcovebods/cli.py
+++ libcovebods/cli.py
@@ -20,6 +20,15 @@ def main():
         "inputfilename", help="File name of an input JSON data file"
     )
 
+    statistics_parser = subparsers.add_parser(
+        "statistics",
+        aliases=["s"],
+        help="",
+    )
+    statistics_parser.add_argument(
+        "inputfilename", help="File name of an input JSON data file"
+    )
+
     additional_fields_parser = subparsers.add_parser(
         "additionalfields",
         aliases=["af"],
@@ -48,6 +57,17 @@ def main():
         output_data = libcovebods.lib.common_checks.process_additional_checks(input_data, config, schema)
 
         print(json.dumps(output_data['additional_checks'], indent=4))
+
+    elif args.subparser_name == "statistics" or args.subparser_name == "s":
+
+        with open(args.inputfilename) as fp:
+            input_data = json.load(fp)
+
+        config = libcovebods.config.LibCoveBODSConfig()
+        schema = libcovebods.schema.SchemaBODS(input_data, config)
+        output_data = libcovebods.lib.common_checks.process_additional_checks(input_data, config, schema)
+
+        print(json.dumps(output_data['statistics'], indent=4))
 
     elif args.subparser_name == "additionalfields" or args.subparser_name == "af":
 
