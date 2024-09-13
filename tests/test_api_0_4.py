ffb381d2adc06ef45aa3e2cca1278124c63c8b64
Ed (ODSC) <ed.lloyd-davies@opendataservices.coop>
2024-09-13 13:22:10 +0100

    tests: Tests for BODS 0.4

--- tests/test_api_0_4.py
+++ tests/test_api_0_4.py
@@ -15,8 +15,6 @@ def test_basic_1():
 
     results = bods_json_output(cove_temp_folder, json_filename)
 
-    print(results)
-
     assert results["schema_version"] == "0.4"
     assert results["validation_errors_count"] == 0
     assert results["additional_fields_count"] == 0
@@ -31,13 +29,13 @@ def test_has_public_listing_information_but_has_public_listing_is_false_1():
     json_filename = os.path.join(
         os.path.dirname(os.path.realpath(__file__)),
         "fixtures",
-        "0.3",
+        "0.4",
         "has_public_listing_information_but_has_public_listing_is_false_1.json",
     )
 
     results = bods_json_output(cove_temp_folder, json_filename)
 
-    assert results["schema_version"] == "0.3"
+    assert results["schema_version"] == "0.4"
     assert results["validation_errors_count"] == 0
     assert results["additional_fields_count"] == 0
 
@@ -60,26 +58,25 @@ def test_entity_sub_type_does_not_align_1():
     json_filename = os.path.join(
         os.path.dirname(os.path.realpath(__file__)),
         "fixtures",
-        "0.3",
+        "0.4",
         "entity_sub_type_does_not_align_1.json",
     )
 
     results = bods_json_output(cove_temp_folder, json_filename)
 
-    assert results["schema_version"] == "0.3"
-    assert results["validation_errors_count"] == 0
+    assert results["schema_version"] == "0.4"
+    assert results["validation_errors_count"] == 1
     assert results["additional_fields_count"] == 0
+    assert results["additional_checks_count"] == 0
 
-    assert results["additional_checks_count"] == 1
     assert (
-        results["additional_checks"][0]["type"]
-        == "statement_entity_type_and_entity_sub_type_do_not_align"
+        results["validation_errors"][0]["message"]
+        == "'trust' is not one of ['other']"
     )
     assert (
-        results["additional_checks"][0]["statement"]
-        == "1dc0e987-5c57-4a1c-b3ad-61353b66a9b7"
+        results["validation_errors"][0]['path_ending']
+        == 'subtype'
     )
-
 
 def test_entity_sub_type_does_align_1():
 
@@ -89,13 +86,13 @@ def test_entity_sub_type_does_align_1():
     json_filename = os.path.join(
         os.path.dirname(os.path.realpath(__file__)),
         "fixtures",
-        "0.3",
+        "0.4",
         "entity_sub_type_does_align_1.json",
     )
 
     results = bods_json_output(cove_temp_folder, json_filename)
 
-    assert results["schema_version"] == "0.3"
+    assert results["schema_version"] == "0.4"
     assert results["validation_errors_count"] == 0
     assert results["additional_fields_count"] == 0
     assert results["additional_checks_count"] == 0
@@ -109,13 +106,13 @@ def test_basic_with_correct_mic_codes_1():
     json_filename = os.path.join(
         os.path.dirname(os.path.realpath(__file__)),
         "fixtures",
-        "0.3",
+        "0.4",
         "basic_with_correct_mic_codes_1.json",
     )
 
     results = bods_json_output(cove_temp_folder, json_filename)
 
-    assert results["schema_version"] == "0.3"
+    assert results["schema_version"] == "0.4"
     assert results["validation_errors_count"] == 0
     assert results["additional_fields_count"] == 0
     assert results["additional_checks_count"] == 0
@@ -129,13 +126,13 @@ def test_basic_with_operating_mic_code_missing_1():
     json_filename = os.path.join(
         os.path.dirname(os.path.realpath(__file__)),
         "fixtures",
-        "0.3",
+        "0.4",
         "basic_with_operating_mic_code_missing_1.json",
     )
 
     results = bods_json_output(cove_temp_folder, json_filename)
 
-    assert results["schema_version"] == "0.3"
+    assert results["schema_version"] == "0.4"
     assert results["validation_errors_count"] == 0
     assert results["additional_fields_count"] == 0
 
@@ -147,5 +144,5 @@ def test_basic_with_operating_mic_code_missing_1():
     assert results["additional_checks"][0]["statement_type"] == "entity"
     assert (
         results["additional_checks"][0]["statement"]
-        == "1dc0e987-5c57-4a1c-b3ad-61353b66a9b7"
+        == "2f7bf9370f1254068e5e946df067d07d"
     )
---
+++ tests/test_schema_validation_0_4.py
@@ -0,0 +1,60 @@
+import json
+import csv
+import os
+import pytest
+import tempfile
+
+from tests.api import bods_json_output
+
+@pytest.fixture
+def expected_errors():
+    expected = []
+    with open('tests/fixtures/0.4/invalid-schema/expected_errors.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            expected.append(row)
+    return expected
+
+def extract_elements(element_path):
+    elems = [0 if elem == '$[0]' else elem for elem in element_path.split(".")]
+    out = []
+    for elem in elems:
+        if isinstance(elem, str) and '[0]' in elem:
+            out.append(elem.split('[0]')[0])
+            out.append(0)
+        else:
+            out.append(elem)
+    if len(out) == 1 and out[0] == '$': out = []
+    return out
+
+def test_all_schema_validation(expected_errors):
+
+    cove_temp_folder = tempfile.mkdtemp(
+        prefix="lib-cove-bods-tests-", dir=tempfile.gettempdir()
+    )
+
+    for expected in expected_errors:
+
+        json_filename = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)), "fixtures", "0.4", "invalid-schema", expected[0]
+        )
+
+        results = bods_json_output(cove_temp_folder, json_filename)
+
+        assert results["schema_version"] == "0.4"
+        if expected[1] in ('enum', 'type'):
+            assert results['validation_errors_count'] > 0
+        else:
+            assert results['validation_errors_count'] == 1
+        assert results['validation_errors'][0]['validator'] == expected[1]
+        assert results['validation_errors'][0]['path'] == extract_elements(expected[2])
+        if expected[1] == 'required':
+            assert results['validation_errors'][0]['extra']['required_key_which_is_missing'] == expected[3]
+        elif expected[1] in ('enum', 'anyOf', 'type', 'maxLength', 'minLength',
+                             'format', 'minimum', 'maximum', 'pattern', 'oneOf',
+                             'const'):
+            pass
+        else:
+            print(expected)
+            print(results['validation_errors'][0])
+            assert False
