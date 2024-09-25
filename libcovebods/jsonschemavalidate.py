8aa4636942447ba15601953045e07aa58b6cdf9b
Ed (ODSC) <ed.lloyd-davies@opendataservices.coop>
2024-09-25 11:17:04 +0100

    libcovebods: Debugging

--- libcovebods/jsonschemavalidate.py
+++ libcovebods/jsonschemavalidate.py
@@ -107,6 +107,7 @@ class JSONSchemaValidator:
             # Get the registry
             registry = self._schema._pkg_schema_obj
 
+            print("Registry:", registry)
             # Make the validator
             statement_schema = registry.contents("urn:statement")
             validator = Draft202012Validator(
